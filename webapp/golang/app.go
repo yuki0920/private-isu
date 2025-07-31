a8199a138fd0b009e0852daf76297a3896524a19
yuki0920 <ywsep20@gmail.com>
2025-07-31 10:52:03 +0900

    Refactor Post struct and update SQL query to include user account names and filter deleted users

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -54,7 +54,7 @@ type Post struct {
 	CreatedAt    time.Time `db:"created_at"`
 	CommentCount int
 	Comments     []Comment
-	User         User
+	User         User `db:""`
 	CSRFToken    string
 }
 
@@ -407,7 +407,12 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 	results := []Post{}
 
 	// TODO: fixme
-	err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC")
+	query := `SELECT p.id, p.user_id, p.body, p.created_at, p.mime, u.account_name
+	FROM posts AS p JOIN users AS u ON p.user_id = u.id
+	WHERE u.del_flg = 0
+	ORDER BY created_at DESC
+	LIMIT 20`
+	err := db.Select(&results, query)
 	if err != nil {
 		log.Print(err)
 		return
