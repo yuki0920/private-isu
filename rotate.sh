3ff75b08bd80907de9ee35347b8de4595aeef885
yuki0920 <ywsep20@gmail.com>
2025-08-02 08:21:08 +0900

    Add favicon.ico location block to Nginx configuration

--- rotate.sh
+++ rotate.sh
@@ -5,6 +5,7 @@ mv webapp/logs/nginx/error.log webapp/logs/nginx/error.log.`date +%Y%m%d%H%M%S`
 mv webapp/logs/mysql/mysql-slow.log webapp/logs/mysql/mysql-slow.log.`date +%Y%m%d%H%M%S`
 
 cd webapp
+rm -rf public/image/*
 docker compose down
 docker compose up --build
 popd
